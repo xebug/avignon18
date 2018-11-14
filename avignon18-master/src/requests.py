a96c754dcd87ddf8435ff279cecd142387a01c94
xebug <michael.rassu@gmail.com>
2018-11-14 18:36:16 +0100

    Delete requests.py

--- avignon18-master/src/requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-if __name__ == "__main__":
-    r = requests.post('http://localhost:5000/student/Lisa', data={})
-    print r.text
-
-    r = requests.get('http://localhost:5000/student/Lisa')
-    print r.text
